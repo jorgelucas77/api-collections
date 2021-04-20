42270f5ae8ce9da659e5642282040862c5b20ada
Jorge Lucas <jorgelucas77@gmail.com>
2021-04-20 15:37:14 +0200

    Map examples

---
+++ src/main/kotlin/one/digitainnovation/collections/TesteMap.kt
@@ -0,0 +1,14 @@
+package one.digitainnovation.collections
+
+fun main() {
+    val pair: Pair<String, Double> = Pair("Joao", 1000.0)
+    val map1 = mapOf(pair)
+
+    map1.forEach{ (k,v) -> println("Chave: $k - Valor: $v") }
+
+    val map2 = mapOf(
+        "Pedro" to 2500.0,
+        "Maria" to 30000.0
+    )
+    map2.forEach{ (k,v) -> println("Chave: $k - Valor: $v") }
+}
